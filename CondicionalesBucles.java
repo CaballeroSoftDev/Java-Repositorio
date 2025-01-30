dc6d8e76d8a10591569561f9048646a8fe459e98
Eduardo <152927064+CaballeroSoftDev@users.noreply.github.com>
2025-01-30 02:39:34 +0000

    add Condicionales y bucles

---
+++ CondicionalesBucles.java
@@ -0,0 +1,17 @@
+public class CondicionalesBucles {
+
+    public static void main(String args[]){
+
+        // Condicional
+        int edad = 10;
+
+        if (edad > 18) {
+            System.out.println("Eres mayor de edad");
+        } else {
+            System.out.println("Eres menor de edad");
+        }
+
+    }
+
+    
+}
